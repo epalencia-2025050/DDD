0c643e1a756698b6048d82327eb3c1bbedb2d680
Epalencia-2025050 <epalencia-2025050@kinal.edu.gt>
2026-03-26 20:00:55 -0600

    feat: se agrega DetalleVentaRepository

---
+++ src/main/java/com/eduardoemilio/KinalApp/repository/DetalleVentaRepository.java
@@ -0,0 +1,9 @@
+package com.eduardoemilio.KinalApp.repository;
+
+import com.eduardoemilio.KinalApp.entity.DetalleVenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
+}
